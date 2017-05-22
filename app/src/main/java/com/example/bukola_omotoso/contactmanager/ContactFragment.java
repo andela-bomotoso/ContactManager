0d66a1604e4f5a95144b6bed3d31b6b12ecae944
Bukola Omotoso <bukola.omotoso@andela.com>
2017-05-22 18:48:00 -0500

    adds necessary resources and fragments

--- app/src/main/java/com/example/bukola_omotoso/contactmanager/ContactFragment.java
+++ app/src/main/java/com/example/bukola_omotoso/contactmanager/ContactFragment.java
@@ -17,6 +17,6 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 }
