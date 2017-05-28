f96557adce8feccfd80c3c78e44426907326c3f0
Bukola Omotoso <bukola.omotoso@andela.com>
2017-05-27 22:40:54 -0500

    implements listeners on activities to interact with fragments

--- app/src/main/java/com/example/bukola_omotoso/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/bukola_omotoso/contactmanager/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.bukola_omotoso.contactmanager;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ContactFragment.ContactFragmentListener,EditAddFragment.EditAddFragmentListener, ContactDetailsFragment.ContactDetailsFragmentListener{
+    public static final String CONTACT_URI = "contact_uri";
+    private ContactFragment contactFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+            contactFragment = new ContactFragment();
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.add(R.id.fragmentContainer, contactFragment);
+            transaction.commit();
     }
 
+    @Override
+    public void onEditAddCompleted(Uri contactUri) {
+        getSupportFragmentManager().popBackStack();
+        contactFragment.updateContactList();
+    }
+
+    @Override
+    public void onContactSelected(Uri contactUri) {
+        displayContact(contactUri, R.id.fragmentContainer);
+    }
+
+    @Override
+    public void onContactAdded() {
+        displayEditAddFragment(R.id.fragmentContainer, null);
+    }
+
+    private void displayContact(Uri contactUri, int viewID) {
+        ContactDetailsFragment detailsFragment = new ContactDetailsFragment();
+        Bundle arguments =  new Bundle();
+        arguments.putParcelable(CONTACT_URI, contactUri);
+        detailsFragment.setArguments(arguments);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(viewID, detailsFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private void displayEditAddFragment(int viewID, Uri contactUri) {
+        EditAddFragment editAddFragment = new EditAddFragment();
+
+        if(contactUri != null)  {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(CONTACT_URI, contactUri);
+            editAddFragment.setArguments(arguments);
+        }
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(viewID, editAddFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onContactDeleted() {
+        getSupportFragmentManager().popBackStack();
+        contactFragment.updateContactList();
+
+    }
+
+    @Override
+    public void onEditContact(Uri contactUri) {
+        displayEditAddFragment(R.id.fragmentContainer, contactUri);
+    }
 }
